358d5074d2d5e519b7d8b329ac7cb0c27fa2bd3b
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-03-04 23:21:21 -0800

    Changed two to three-stream full C3D

--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-/home/lferrer/Downloads/caffe/build/tools/caffe \
+~/c3d/C3D-v1.1/build/install/bin/caffe \
   train \
-  --solver=solver.prototxt \
+  --solver=solver.prototxt --gpu=1
+  --weights=../conv3d_deepnetA_sport1m_iter_1900000?dl=0 \
   $@ \
   2>&1 | tee min_test.log
