f5ef84dcc044cbbb1a59a8b18f5f991e501d35f3
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-03-29 13:34:17 -0700

    Adding custom triplet loss

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-./c3d/C3D-v1.1/build/install/bin/caffe \
+./C3D-v1.1/build/tools/caffe \
 train \
 --solver=solver.prototxt \
---weights=../conv3d_deepnetA_sport1m_iter_1900000 \
+--weights=../c3d_resnet18_sports1m_r2_iter_2800000.caffemodel \
 2>&1 | tee mini_test.log
