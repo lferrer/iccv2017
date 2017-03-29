73f801f4ad1bb9e57341958b8ff8915975c89536
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-03-29 12:02:08 -0700

    Rebasing C3D for finetuning

--- train.sh
+++ train.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
-set -e
-
-~/c3d/C3D-v1.1/build/install/bin/caffe \
-  train \
-  --solver=solver.prototxt --gpu=1
-  --weights=../conv3d_deepnetA_sport1m_iter_1900000?dl=0 \
-  2>&1 | tee mini_test.log
+./c3d/C3D-v1.1/build/install/bin/caffe \
+train \
+--solver=solver.prototxt \
+--weights=../conv3d_deepnetA_sport1m_iter_1900000 \
+2>&1 | tee mini_test.log
