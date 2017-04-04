cc63f502e24f1c21bc7ea7631f3c99b20feb570b
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-04-04 09:49:27 -0700

    Added UCF-101 model

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-./C3D-v1.1/build/install/bin/caffe \
+./caffe/build/install/bin/caffe \
 train \
 -solver=solver.prototxt \
--weights=../c3d_resnet18_sports1m_r2_iter_2800000.caffemodel \
+-weights=../c3d_ucf101_iter_38000.caffemodel \
 2>&1 | tee mini_test.log
