ed15f6198ff6a5161d3470e793836ffc580aeb0e
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-04-12 16:26:52 -0700

    Uploading initial training results

--- scripts/compute_NN.py
+++ scripts/compute_NN.py
@@ -9,42 +9,15 @@ import numpy as np
 import caffe
 
 CLIP_LENGTH = 16
-VIDEO_LENGTH = 4292
-SCENARIOS = ['Fire Pit', 'Grass Standard', 'Ice Natural', 'Reflection Agent', 'Temple Barbarous',
-             'Fire Natural', 'Grass Pit', 'Ice Barbarous', 'Reflection Standard', 'Temple Agent']
-ELEVATION_ANGLES = [330, 340, 350]
-ROTATION_ANGLES = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, \
-                   200, 220, 240, 260, 280, 300, 320, 340]
-DS_ROOT = '/home/lferrer/Documents/Synthetic'
+DS_ROOT = '/data/leo-data/Synthetic/NN/'
 # Video frames were to 128 by 171 (According to original paper)
 FRAME_WIDTH = 128
 FRAME_HEIGHT = 171
 MODEL = '../models/three_stream_deploy.prototxt'
 WEIGHTS = '../weights/c3d_ucf101_iter_1000.caffemodel'
-BATCH_SIZE = 48
-TEST_ITERATIONS = 10
-
-# Variables
-hit_dict = {}
+BATCH_SIZE = 3
 
 # Helper functions
-# Returns the filename of a given image based on the dataset structure
-def build_filename(first_person, scenario, index, elevation=330, rotation=0):
-    if first_person:
-        # First-Person goes from 1 - 4192
-        image_name = str(index + 1) + '.jpg'
-        filename = os.path.join(DS_ROOT, 'First Person', scenario, image_name)
-    else:
-        # Third person goes from 0 to 4191
-        image_name = str(index) + '.jpg'
-        angle_folder = str(elevation) + '-' + str(rotation)
-        filename = os.path.join(DS_ROOT, 'Third Person', scenario, angle_folder, image_name)
-    return filename
-
-# Returns a random element from a list
-def get_rnd_el(my_list):
-    return my_list[randint(0, len(my_list) - 1)]
-
 def get_base_filename(filename):
     sub_filename = filename[-10:]
     slash_index = string.find(sub_filename, '/')
@@ -52,16 +25,6 @@ def get_base_filename(filename):
     image_file_length = 5 - slash_index + 4
     base_filename = filename[:-image_file_length]
     return base_filename, index
-
-def get_random_sample(fp_sample):
-    scenario = get_rnd_el(SCENARIOS)
-    index = randint(1, VIDEO_LENGTH - CLIP_LENGTH - 1)
-    if fp_sample:
-        return build_filename(True, scenario, index)
-    else:
-        elevation = get_rnd_el(ELEVATION_ANGLES)
-        rotation = get_rnd_el(ROTATION_ANGLES)
-        return build_filename(False, scenario, index, elevation, rotation)
 
 def load_image_sample(sample_filename):
     # Retrieve the index number
@@ -93,6 +56,13 @@ def load_image_sample(sample_filename):
 
     return sample_image_data, sample_index
 
+if len(sys.argv) < 2:
+    print "Error: Not enough parameters given. Parameters needed: -filenames_file -features_path"
+    exit()
+else:
+    filenames_file = sys.argv[1]
+    features_path = sys.argv[2]
+
 # Start Caffe
 caffe.set_mode_cpu()
 net = caffe.Net(MODEL, WEIGHTS, caffe.TEST)
@@ -100,36 +70,37 @@ net = caffe.Net(MODEL, WEIGHTS, caffe.TEST)
 # build the mean object
 mu = [('B', 67.1991691), ('G', 75.33919712), ('R', 90.82602441)]
 
-
 # set the size of the input (we can skip this if we're happy
 #  with the default; we can also change it later, e.g., for different batch sizes)
 net.blobs['data'].reshape(BATCH_SIZE,        # batch size
                           3,         # 3-channel (BGR) images
                           227, 227)  # image size is 227x227
-fp_sample = True
-for i in range(TEST_ITERATIONS):
-    image_data = np.array([], dtype=np.uint8).reshape(3, FRAME_WIDTH, FRAME_HEIGHT)
 
+# Get a pointer to the file
+image_filenames = [line.rstrip('\n') for line in open(filenames_file, "r")]
+features_file = open(features_path, "w")
+
+for index in range(len(image_filenames)):
+    image_data = np.empty([FRAME_HEIGHT, FRAME_WIDTH, 3*CLIP_LENGTH], dtype=np.uint8)
+    image_filename = DS_ROOT + image_filenames[index]
     # Generate a batch
     for j in range(BATCH_SIZE):
-        sample_filename = get_random_sample(fp_sample)
-        sample_image_data, sample_index = load_image_sample(sample_filename)
-        while sample_filename in hit_dict and sample_index == -1:
-            sample_filename = get_random_sample(fp_sample)
-            sample_image_data, sample_index = load_image_sample(sample_filename)
-
-        # Add the random image to the used list
-        hit_dict[sample_filename] = True
+        sample_image_data, sample_index = load_image_sample(image_filename)
+        while sample_index == -1:
+            index = index + 1
+            image_filename = DS_ROOT + image_filenames[index]
+            sample_image_data, sample_index = load_image_sample(image_filename)
 
         # Stack the images over the depth dimension
+#        print image_data.shape, sample_image_data.shape
         image_data = np.dstack((sample_image_data, image_data))
-
-        # Invert to generate another type of sample
-        fp_sample = not fp_sample
 
     # copy the image data into the memory allocated for the net
     net.blobs['data'].data[...] = image_data
     output = net.forward()
-    
+    features_file.write(output + "\n")
+
+list_file.close()
+features_file.close()
 
 
