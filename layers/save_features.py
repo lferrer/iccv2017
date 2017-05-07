060b766efac88cf5c458c7a906fa0683c7126872
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-05-07 16:48:36 -0700

    Posting training results

--- layers/save_features.py
+++ layers/save_features.py
@@ -8,25 +8,31 @@ class SaveFeaturesLayer(caffe.Layer):
         if len(bottom) == 0:
             raise Exception("Need at least one input")
         if hasattr(self, 'param_str') and self.param_str:
-            self.n_samples = int(self.param_str[0])
+            self.n_samples = int(self.param_str)
         else:
             raise Exception("Need to setup param_str")
         self.sample_index = 0
     def reshape(self, bottom, top):
         self.n_inputs = len(bottom)
         self.batch_size = len(bottom[0].data)
-        self.features = np.empty([self.n_inputs,
-                                  self.n_samples,
-                                  len(bottom[0].data[0])],
-                                 dtype=float)
+        if not hasattr(self, 'features'):
+            self.features = np.empty([self.n_inputs,
+                                      self.n_samples,
+                                      len(bottom[0].data[0])],
+                                     dtype=float)
+        #print "2132342134244234"
 
     def forward(self, bottom, top):
         for i in range(self.n_inputs):
             for j in range(self.batch_size):
                 self.features[i][self.sample_index + j] = bottom[i].data[j]
+                #print bottom[i].data[j]
+                #print self.features[i][self.sample_index + j]
+        #print np.nonzero(self.features[0][0])
         self.sample_index = self.sample_index + self.batch_size
         if self.sample_index == self.n_samples:
-            np.savez_compressed('features.npz', self.features)
+            np.savez_compressed('../../features/features.npz', self.features)
+	    print 'Features saved'
 
     def backward(self, top, propagate_down, bottom):
         pass
--- scripts/testing/compute_features.sh
+++ scripts/testing/compute_features.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 ../../caffe/build/install/bin/caffe \
 test \
--model=../../models/three_stream_deploy.prototxt -gpu=2 \
+-model=../../models/three_stream_deploy.prototxt -gpu=7 \
 -weights=../../../c3d_ucf101_iter_38000.caffemodel \
--iterations=15000 \
-2>&1 | tee ../../logs/three_stream_triplet_loss_features.log
+-iterations=1500 \
+2>&1 | tee ../../logs/three_stream_triplet_loss_features_test.log
 
