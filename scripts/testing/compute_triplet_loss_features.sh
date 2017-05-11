c51297f943a42731bdb6205160f9ffe9ef763253
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-05-11 13:31:32 -0700

    Adding features logs

--- scripts/testing/compute_triplet_loss_features.sh
+++ scripts/testing/compute_triplet_loss_features.sh
@@ -2,7 +2,7 @@
 ../../caffe/build/install/bin/caffe \
 test \
 -model=../../models/three_stream_deploy.prototxt -gpu=7 \
--weights=../../../three_stream_triplet_loss_iter_60000.caffemodel \
--iterations=1500 \
-2>&1 | tee ../../logs/triplet_loss_features.log
+-weights=../../weights/three_stream_triplet_loss_iter_60000.caffemodel \
+-iterations=1000 \
+2>&1 | tee ../../logs/triplet_loss_features_test.log
 
