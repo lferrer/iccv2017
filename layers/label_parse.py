43d7a1200792e80b6bdf2a1295f9e59613490fed
Leonardo Ferrer <ferrer@ucdavis.edu>
2017-05-24 08:18:54 -0700

    Adding label parsing

---
+++ layers/label_parse.py
@@ -0,0 +1,29 @@
+import caffe
+
+class LabelParseLayer(caffe.Layer):
+    def setup(self, bottom, top):
+        if hasattr(self, 'param_str') and self.param_str:
+            if self.param_str == 'Scene':
+                self.l_type = True
+            elif self.param_str == 'Action':
+                self.l_type = False
+            else:
+                raise Exception("Unsupported label type: " + self.param_str)
+        else:
+            raise Exception("Need to setup param_str")
+
+    def reshape(self, bottom, top):
+        top[0].reshape(len(bottom[0].data), 1)
+
+    def forward(self, bottom, top):
+        for i, label in enumerate(bottom[0].data):
+            print label
+            label = str(label)
+            if self.l_type:
+                my_value = label[:len(label) - 2]
+            else:
+                my_value = label[-2:]
+            top[0].data[i] = int(my_value)
+
+    def backward(self, top, propagate_down, bottom):
+        pass
--- layers/label_split.py
+++ layers/label_split.py
@@ -2,28 +2,50 @@ import caffe
 
 class LabelSplitLayer(caffe.Layer):
     def setup(self, bottom, top):
-        if hasattr(self, 'param_str') and self.param_str:
-            if self.param_str == 'Scene':
-                self.l_type = True
-            elif self.param_str == 'Action':
-                self.l_type = False
-            else:
-                raise Exception("Unsupported label type: " + self.param_str)
-        else:
-            raise Exception("Need to setup param_str")
+        # check input pair
+        if len(bottom) != 1:
+            raise Exception("Need access to the raw_label only.")
+        # check output pair
+        if len(top) != 2:
+            raise Exception("Need two output labels.")
+        # Specifying type of input data
+        self.first_person = True
+
 
     def reshape(self, bottom, top):
+        # check input dimensions match
         top[0].reshape(len(bottom[0].data), 1)
+        top[1].reshape(len(bottom[0].data), 1)
 
     def forward(self, bottom, top):
-        for i, label in enumerate(bottom[0].data):
-            print label
-            label = str(label)
-            if self.l_type:
-                my_value = label[:len(label) - 2]
+        for i, raw_label in enumerate(bottom[0].data):
+            if raw_label < 1000000:
+                #6 digits label
+                label_str = '{0:06.0f}'.format(raw_label)
+                left_label = raw_label[:3]
+                right_label = raw_label[3:]
+            elif raw_label < 10000000:
+                #7 digits label
+                label_str = '{0:07.0f}'.format(raw_label)
+                if label_str[:2] == '10':
+                    # left is longer
+                    left_label = raw_label[:4]
+                    right_label = raw_label[4:]
+                else:
+                    # right is longer
+                    left_label = raw_label[:3]
+                    right_label = raw_label[3:]
             else:
-                my_value = label[-2:]
-            top[0].data[i] = int(my_value)
+                # 8 digits label
+                label_str = '{0:08.0f}'.format(raw_label)
+                left_label = raw_label[:4]
+                right_label = raw_label[4:]
+            if self.first_person:
+                top[0].data[i] = int(left_label)
+                top[1].data[i] = int(right_label)
+            else:
+                top[0].data[i] = int(right_label)
+                top[1].data[i] = int(left_label)
 
     def backward(self, top, propagate_down, bottom):
         pass
